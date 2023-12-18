f8e599dee0bd1dc46568063409f3192c7d60dcc9
Heesho <hisham11027@gmail.com>
2023-12-18 11:17:32 -0800

    added settlement

--- contracts/lsTOKEN/ACTOKEN.sol
+++ contracts/lsTOKEN/ACTOKEN.sol
@@ -8,10 +8,10 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 contract ACTOKEN is ERC20, ReentrancyGuard  {
     using SafeERC20 for ERC20;
 
-    uint256 public constant EMISSION = 1048576; 
+    uint256 public constant EMISSION = 1048576 * 1e18; 
     uint256 public constant EPOCH = 86400;      // 1 day
-    uint256 public constant ERA = 1814400;      // 3 weeks per era
-    uint256 public constant SPAN = 21;          // 21 eras total
+    uint256 public constant ERA = 1814400;      // 21 days per era
+    uint256 public constant SPAN = 21;          // 21 eras
 
     uint256 public immutable genesis;   // genesis epoch time
     uint256 public immutable terminus;  // terminus epoch time
@@ -22,6 +22,7 @@ contract ACTOKEN is ERC20, ReentrancyGuard  {
 
     mapping(uint256=>uint256) epoch_TotalUnits;                         // epoch -> total units
     mapping(uint256=>mapping(address=>uint256)) epoch_Account_Units;    // epoch -> account -> units
+    mapping(uint256=>mapping(address=>bool)) epoch_Account_Settled;     // epoch -> account -> claimed
 
     mapping(uint256=>uint256) epoch_TotalAccounts;  // epoch -> total number of accounts
     mapping(uint256=>address[]) epoch_Accounts;     // epoch -> accounts[]
@@ -32,8 +33,11 @@ contract ACTOKEN is ERC20, ReentrancyGuard  {
     error ACTOKEN__InvalidZeroInput();
     error ACTOKEN__BeforeGenesis();
     error ACTOKEN__AfterTerminus();
+    error ACTOKEN__AlreadyClaimed();
+    error ACTOKEN__EpochActive();
 
     event ACTOKEN__Contribution(address indexed account, uint256 indexed epoch, uint256 amount);
+    event ACTOKEN__Settle(address indexed account, uint256 indexed epoch, uint256 amount);
 
     constructor(address _base) 
         ERC20("acTOKEN", "TOKEN Accumulator")
@@ -42,6 +46,10 @@ contract ACTOKEN is ERC20, ReentrancyGuard  {
         terminus = genesis + (ERA * SPAN);
         base = _base;
         treasury = msg.sender;
+    }
+
+    function getEpoch(uint256 timestamp) public pure returns (uint256) {
+        return (timestamp / EPOCH) * EPOCH;
     }
 
     function getCurrentEpoch() public view returns (uint256) {
@@ -68,6 +76,19 @@ contract ACTOKEN is ERC20, ReentrancyGuard  {
         totalContribution += amount;
         emit ACTOKEN__Contribution(account, epoch, amount);
         IERC20(base).transferFrom(msg.sender, treasury, amount);
+    }
+
+    function settle(address account, uint256 epoch) nonReentrant external {
+        if (getEpoch(block.timestamp) <= epoch) revert ACTOKEN__EpochActive();
+        if (epoch < genesis) revert ACTOKEN__BeforeGenesis();
+        if (epoch > terminus) revert ACTOKEN__AfterTerminus();
+        if (epoch_Account_Settled[epoch][account]) revert ACTOKEN__AlreadyClaimed();
+
+        uint256 emissionRate = EMISSION / (2 ** getCurrentEra());
+        uint256 amount = (epoch_Account_Units[epoch][account] * emissionRate) / epoch_TotalUnits[epoch];
+        epoch_Account_Settled[epoch][account] = true;
+        _mint(account, amount);
+        emit ACTOKEN__Settle(account, epoch, amount);
     }
     
 }
